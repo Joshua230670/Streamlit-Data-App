04a73694418b77134f0d5564b8659392830b86d8
Joshua Izquierdo <114187182+Joshua230670@users.noreply.github.com>
2026-01-29 16:57:27 -0500

    Refactor file path and enhance comments in DataApp.py
    Updated file path handling and improved comments for clarity.

--- DataApp.py
+++ DataApp.py
@@ -21,30 +21,30 @@ category = st.selectbox(
     file_list
 )
 
-acnh_df = pd.read_csv("//Users/joshuaizquierdo/.cache/kagglehub/datasets/jessicali9530/"
-                      "animal-crossing-new-horizons-nookplaza-dataset/versions/3/"
-                      f"{category}") # Create Dataframe
+acnh_df = pd.read_csv(file_path + category) # Create Dataframe
 
 tab1, tab2 = st.tabs(["Table", "Bar Chart"])
 
 with tab1:
+    # Lets users select what .csv file to view
     options = st.multiselect(
         "Select what items you want to hide",
         acnh_df.keys()
     )
 
+    # Display .csv file as a table
     st.dataframe(acnh_df, hide_index=True, column_order=options)
 
 with tab2:
     col1, col2 = st.columns(2)
 
-    with col1:
+    with col1: # Determines the x-axis of the bar graph
         x_axis = st.selectbox(
             "Choose the x-axis",
             acnh_df.keys()
         )
 
-    with col2:
+    with col2: # Determines the y-axis of the bar graph
         y_axis = st.selectbox(
             "Choose the y-axis",
             acnh_df.keys()
@@ -56,4 +56,4 @@ with tab2:
         y=y_axis
     )
 
-    st.plotly_chart(fig1)
+    st.plotly_chart(fig1) # Display bar graph
